1d4e1b9e55999cc2ac9ca6d7b6e32495d93ffc21
Dartanian Bagby <mighty.chess@gmail.com>
2017-10-29 17:16:38 -0700

    hi

---
+++ key.js
@@ -0,0 +1,13 @@
+
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'UjdQf8dOejo4QjPJMrRmY0yl4',
+  consumer_secret: 'TKWmu48fu9vHsE5Fex4HtYVNahFuFw7duTv9OQTJSzxlZJDfob',
+  access_token_key: '921913506152067073-4QhltzCZhNrloVrBOmGKI79SzeybIYH',
+  access_token_secret:'sJT59leM03t5bNBGWS2UL93bpg9MRRNk8dhFvcCLfSIUd',
+
+}
+
+module.exports = twitterKeys;
